cfa48d216af80bf172a76253ae1b58a8132d0de5
WhoeverReadsThisIsStupid <2012k.j.c16@gmail.com>
2022-05-18 23:13:35 +0200

    I have no idea how this shit works

--- src/JAGRE.cpp
+++ src/JAGRE.cpp
@@ -1,12 +1,9 @@
-﻿// JAGRE.cpp: Definiert den Einstiegspunkt für die Anwendung.
-//
+﻿// JAGRE.cpp: Startingpoint of this Engine
 
 #include "JAGRE.h"
 
-using namespace std;
-
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << "Hello CMake." << std::endl;
 	return 0;
 }
