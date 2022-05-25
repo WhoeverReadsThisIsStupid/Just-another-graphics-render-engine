8f7a92d0fc29614ef16eda3b2eaea0db130d30c5
WhoeverReadsThisIsStupid <2012k.j.c16@gmail.com>
2022-05-25 12:31:41 +0200

    Some Progress Hurray! Vector-Math seems to work \o/

--- src/JAGRE.cpp
+++ src/JAGRE.cpp
@@ -4,6 +4,24 @@
 
 int main()
 {
-	std::cout << "Hello CMake." << std::endl;
+	std::cout << "Hello JAGRE." << std::endl;
+	Math::Point point1 = Math::Point(1,2,1);
+	Math::Point point2 = Math::Point(0, 4, 4);
+	Math::Vector vector1 = Math::Vector(2, 0, 0);
+
+	Math::Vector vector2;
+
+	point1.drawPoint();
+	point2.drawPoint();
+
+	vector2 = point1 - point2;
+	vector1 += vector2;
+
+	point1 += vector1;
+	point1.drawPoint();
+
+	point2 -= vector2;
+	point2.drawPoint();
+
 	return 0;
 }
--- src/JAGRE.h
+++ src/JAGRE.h
@@ -1,6 +1,4 @@
 ﻿// JAGRE.h: Main header. Include all the stuff here you want to use!!
 
 #pragma once
-
-#include <iostream>
-#include "Math/MyVector.h"
+#include "Math/Cartesian3D.h"
